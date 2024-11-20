11a4cba33162d78433adb196c60744393d4323fa
Sujay <sujaydey641@gmail.com>
2024-11-20 13:39:32 +0530

    test title

--- src/main.py
+++ src/main.py
@@ -121,8 +121,8 @@ def main(args: Optional[List[str]]=None) -> None:
         org_name, repo_name, discussion_category)
 
     for reviewer_name, pr_list in reviewer_to_assigned_prs.items():
-        # discussion_title = f"Pending Reviews: @{reviewer_name}"
-        discussion_title = "test-123"
+        discussion_title = f"Pending Reviews: {reviewer_name}"
+        # discussion_title = "test-123"
         discussion_body = generate_message('\n'.join(str(pr) for pr in pr_list), TEMPLATE_PATH)
         github_services.create_discussion(
             org_name, repo_name, discussion_category, discussion_title, discussion_body)
